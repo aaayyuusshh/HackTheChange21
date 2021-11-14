0f0e9cccc220545963766cd15a8d17059771c0fd
Mikail Munir <mikailmunir01@gmail.com>
2021-11-14 05:15:08 -0700

    Finished timer

--- src/js/pomodoroScript.js
+++ src/js/pomodoroScript.js
@@ -1,22 +1,33 @@
 var minutes = parseInt(window.prompt("Please enter the amount of minutes you would like to study today: "));
 var seconds = "00";
+var minutesInterval;
 var breaks = parseInt(window.prompt("Please enter the amount of minutes you would like your breaks to be: "));
 var countStudy = 0;
 var countBreak = 0;
 var breakTime = false;
+var notify = new Audio("/src/js/breakTime.mp3");
 
 function init(){
+    minutes = (minutes < 25 || minutes != typeof(Number)) ? 25 : minutes;
+    breaks = (breaks < 5 || breaks != typeof(Number)) ? 5 : breaks;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 }
 
+function player(){
+    if(minutesInterval === undefined){
+        startTime();
+    } 
+}
+
 function startTime(){
     minutes -= 1;
+    document.getElementById("minutes").innerHTML = (minutes < 10) ? ("0" + minutes) : minutes;
     seconds = 59;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 
-    var minutesInterval = setInterval(minutesElapsed, 60000);
+    minutesInterval = setInterval(minutesElapsed, 60000);
     var secondsInterval = setInterval(secondsElapsed, 1000);
 
     function minutesElapsed(){
@@ -30,15 +41,21 @@ function startTime(){
         }
 
         if((countStudy > 0) && (countStudy % 2) === 0){
-            breakTime = true;
-            document.getElementById("finished").innerHTML = `Break time! It's time to do something de-stressing for the next ${breaks} minutes.`;
-            document.getElementById("finished").classList().add("finishMessage");
+            if(breakTime === false){
+                breakTime = true;
+                notify.play();
+                document.getElementById("finished").innerHTML = `Break time! It's time to de-stress for the next ${breaks} minutes.`;
+                document.getElementById("finished").classList.add("finishMessage");
+            }
         }
     
         if((countBreak > 0) && (countBreak % breaks) === 0){
-            breakTime = false;
-            document.getElementById("finished").innerHTML = "Break time over! Time to get back to work.";
-            document.getElementById("finished").classList().add("finishMessage");
+            if(breakTime === true){
+                breakTime = false;
+                notify.play();
+                document.getElementById("finished").innerHTML = "Break time over! Time to get back to work.";
+                document.getElementById("finished").classList.add("finishMessage");
+            }
         }
     }
     function secondsElapsed(){
@@ -47,14 +64,14 @@ function startTime(){
 
         if(seconds <= 0){
             if(minutes <= 0){
+                notify.play();
                 clearInterval(minutesInterval);
                 clearInterval(secondsInterval);
                 document.getElementById("finished").innerHTML = "Session complete! You have finished your work for the day!";
-                document.getElementById("finished").classList().add("finishMessage");
+                document.getElementById("finished").classList.add("finishMessage");
 
             }
             seconds = 60;
         }
     }
-
 }
